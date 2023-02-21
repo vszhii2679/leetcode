786a323bd0d26f295e7d3775b52d25527405800b
sobann <sobann@163.com>
2023-02-21 17:21:24 +0800

    p876(链表的中间结点)

---
+++ src/main/java/com/cheese/leetcode/editor/cn/P876MiddleOfTheLinkedList.java
@@ -0,0 +1,100 @@
+package com.cheese.leetcode.editor.cn;
+
+/*
+Java：链表的中间结点
+------------------------
+//给定一个头结点为 head 的非空单链表，返回链表的中间结点。 
+//
+// 如果有两个中间结点，则返回第二个中间结点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[1,2,3,4,5]
+//输出：此列表中的结点 3 (序列化形式：[3,4,5])
+//返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+//注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+//ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = 
+//NULL.
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：[1,2,3,4,5,6]
+//输出：此列表中的结点 4 (序列化形式：[4,5,6])
+//由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 给定链表的结点数介于 1 和 100 之间。 
+// 
+//
+// 👍 801 👎 0
+
+ */
+public class P876MiddleOfTheLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new P876MiddleOfTheLinkedList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            return middleNode_(head);
+        }
+
+        /**
+         * 快慢指针
+         * pb指针运行的速度为pa的两倍
+         * 当pb或 pb.next指向null时，此时pa指向的位置就是链表的中点
+         *
+         * @param head
+         * @return
+         */
+        public ListNode middleNode_(ListNode head) {
+            ListNode pa = head;
+            ListNode pb = head;
+            while (pa != null && pb != null && pb.next != null) {
+                pa = pa.next;
+                pb = pb.next.next;
+            }
+            return pa;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
